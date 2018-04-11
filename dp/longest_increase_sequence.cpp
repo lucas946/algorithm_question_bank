a97c6d4aefb87c229ff7314397749db9b2937f2f
lucas <martinnnn94@gmail.com>
2018-04-11 22:03:16 +0800

    longest_increase_sequence.cpp

---
+++ dp/longest_increase_sequence.cpp
@@ -0,0 +1,37 @@
+/* 题目描述：
+ * 给定一个序列，求该序列的最长非降子序列的长度
+ *
+ * 状态定义：dp[i]以i结尾的最长非降子序列的长度>=1
+ * 状态转移方程：dp[i]=max{dp[j]+1}，其中，j<i,seq[j]>seq[i]
+ */
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int LIS(vector<int> &sequences){
+	int length = sequences.size();
+	int dp[length];
+	for(int i=0;i<length;++i)
+		dp[i] = 1;
+	for(int i=1;i<length;++i){
+		int max = 0;
+		for(int j=0;j<=i-1;++j){
+			if(j<i&&sequences[j]<=sequences[i]&&dp[j]>max){
+				max = dp[j];
+				dp[i] = dp[j]+1;
+			}
+		}
+	}
+	sort(dp,dp+length);
+	return dp[length-1];
+}
+
+int main(){
+	int numbers[] = {6,5,4,3,2,1};
+	vector<int> seq(numbers,numbers+6);
+	int result = LIS(seq);
+	cout<<result<<endl;
+
+	return 0;
+}
