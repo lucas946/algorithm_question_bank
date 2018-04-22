71bd592e40d35b2d1ae03bdb671626e06e70170e
lucas <martinnnn94@gmail.com>
2018-04-22 21:38:39 +0800

    modified binarySearch.cpp

--- search/binarySearch.cpp
+++ search/binarySearch.cpp
@@ -12,6 +12,7 @@
 
 using namespace std;
 
+//递归实现
 int binary_search(const vector<int> &nums, int low, int high, int target){
 	if(low>high)
 		return -1;
@@ -25,6 +26,27 @@ int binary_search(const vector<int> &nums, int low, int high, int target){
 
 	return mid;
 }
+
+// 非递归实现
+/* 所有的递归都可以自行定义stack来解递归，所以二分查找法也可以不用递归实现，
+ * 而且它的非递归实现甚至可以不用栈，因为二分的递归其实是尾递归，它不关心递
+ * 归前的所有信息。
+ */
+
+/*
+int binary_search(const vector<int> &nums, int low, int high, int target){
+	while(low<=high){
+		int mid = (low+high)/2;
+		if(nums[mid]>target)
+			high = mid - 1;
+		else if(nums[mid]<target){
+			low = mid +1;
+		else
+			return mid;
+	}
+	return -1;
+}
+*/
 
 int bsearch(const vector<int> &nums, int target){
 	int size = nums.size();
